7c2460503560f38bd36475afc106aa394ac1f700
evgnskv <evgnskv@pm.me>
2025-08-22 17:13:17 +0900

    undo stow structure

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+export XDG_CONFIG_HOME="$HOME"/.config
 export NIXPKGS_ALLOW_UNFREE=1
 
-mkdir -p "$HOME"/.config/nixpkgs
-ln -sf "$HOME"/dotfiles/nix/.config/nixpkgs/config.nix \
-       "$HOME"/.config/nixpkgs/config.nix
+mkdir -p "$XDG_CONFIG_HOME"/nixpkgs
+
+ln -sf "$PWD"/config.nix \
+       "$XDG_CONFIG_HOME"/nixpkgs/config.nix
+ln -sf "$PWD"/nvim \
+       "$XDG_CONFIG_HOME"/nvim
+
+ln -sf "$PWD"/.bashrc \
+       "$HOME"/.bashrc
+ln -sf "$PWD"/.bash_aliases \
+       "$HOME"/.bash_aliases
+ln -sf "$PWD"/.tmux.conf \
+       "$HOME"/.tmux.conf
 
 nix-env -iA nixpkgs.myPackages
 
-cd "$HOME"/dotfiles && stow .
